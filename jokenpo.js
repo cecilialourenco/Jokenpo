bcfffdb91321cac1ce71a4df37309a7c845d054e
cecilia <cecilia.lourenco@contavoltz.com>
2021-12-22 23:13:43 -0300

    exibir resultado da jogada

--- jokenpo.js
+++ jokenpo.js
@@ -1,4 +1,11 @@
 const options = document.querySelectorAll(".play-options button");
+const playButton = document.getElementById("play-button");
+const playTransition = document.getElementById("play-transition");
+const playResults = document.getElementById("play-results");
+const choicePlayer1 = document.getElementById("choice-player1");
+const choicePlayer2 = document.getElementById("choice-player2");
+const choiceWinner = document.getElementById("choice-winner");
+const winnerAnnouncement = document.getElementById("winner-announcement");
 
 for (let o of options) {
   o.addEventListener("click", select);
@@ -36,5 +43,51 @@ function winner (optionPlayer1, optionPlayer2) {
       return 2;
     }
 }
-//   }
-// }
+
+function play () {
+  const optionPlayer1 = document.querySelector(".play-options.player1 .selected");
+  const optionPlayer2 = document.querySelector(".play-options.player2 .selected");
+  playTransition.classList.remove("hidden");
+  playButton.classList.add("hidden");
+  results(optionPlayer1, optionPlayer2);
+  setTimeout(() => {
+    playTransition.classList.add("hidden");
+    playResults.classList.remove("hidden");
+  }, 1000);
+
+  setTimeout(() => {
+    playResults.classList.add("hidden");
+    winnerAnnouncement.classList.remove("hidden");
+  }, 2000);
+
+  setTimeout(() => {
+    winnerAnnouncement.classList.add("hidden");
+    playButton.classList.remove("hidden");
+  }, 4000);
+}
+
+function results (optionPlayer1, optionPlayer2) {
+  choicePlayer1.innerHTML = "";
+  choicePlayer1.appendChild(optionPlayer1.children[0].cloneNode(true));
+  choicePlayer2.innerHTML = "";
+  choicePlayer2.appendChild(optionPlayer2.children[0].cloneNode(true));
+  const playerWinner = winner(optionPlayer1.name, optionPlayer2.name);
+  const message = document.querySelector("#winner-announcement p");
+  if (playerWinner === 1) {
+    choiceWinner.innerHTML = "";
+    choiceWinner.appendChild(optionPlayer1.children[0].cloneNode(true));
+    message.textContent = "Player 1 venceu";
+    choiceWinner.className = "win";
+  } else if (playerWinner === 2) {
+    choiceWinner.innerHTML = "";
+    choiceWinner.appendChild(optionPlayer2.children[0].cloneNode(true));
+    message.textContent = "Player 2 venceu";
+    choiceWinner.className = "win";
+  } else {
+    choiceWinner.innerHTML = "";
+    choiceWinner.appendChild(optionPlayer1.children[0].cloneNode(true));
+    message.textContent = "Empate"
+    choiceWinner.className = "draw";
+  }
+}
+
