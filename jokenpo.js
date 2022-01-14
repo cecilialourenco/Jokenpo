d784c4df4b8104e3b803bd287e817714b34ae222
cecilia <cecilia.lourenco@contavoltz.com>
2022-01-13 21:58:07 -0300

    implementation of the new rules of the game

--- jokenpo.js
+++ jokenpo.js
@@ -7,6 +7,13 @@ const choiceCPU = document.getElementById("choice-cpu");
 const choiceWinner = document.getElementById("choice-winner");
 const winnerAnnouncement = document.getElementById("winner-announcement");
 const optionsCPU = document.querySelector(".play-options.cpu").children;
+let placarPlayer1 = 0;
+let placarCPU = 0;
+const finalAnnouncement = document.getElementById("final-announcement");
+let gameOver = false;
+const player1ScoreElement = document.getElementById("player1-score");
+const cpuScoreElement = document.getElementById("cpu-score");
+const optionsPlayer1 = document.querySelector(".play-options.player1");
 
 for (let o of options) {
   o.addEventListener("click", select);
@@ -47,10 +54,17 @@ function playCPU(){
 }
 
 function play() {
+  if (gameOver == true){
+    placarPlayer1 = 0;
+    placarCPU = 0;
+    updateScore();
+    finalAnnouncement.textContent = '';
+    gameOver = false;
+  }
   const optionPlayer1 = document.querySelector(
     ".play-options.player1 .selected"
   );
-  
+
   playCPU()
   const optionCPU = document.querySelector(
     ".play-options.cpu .selected"
@@ -67,11 +81,13 @@ function play() {
   setTimeout(() => {
     playResults.classList.add("hidden");
     winnerAnnouncement.classList.remove("hidden");
+    updateScore();
   }, 2000);
 
   setTimeout(() => {
     winnerAnnouncement.classList.add("hidden");
     playButton.classList.remove("hidden");
+    optionsPlayer1.classList.remove("hidden");
   }, 5000);
 }
 
@@ -88,12 +104,12 @@ function results(optionPlayer1, optionCPU) {
   if (playerWinner === 1) {
     choiceWinner.innerHTML = "";
     choiceWinner.appendChild(optionPlayer1.children[0].cloneNode(true));
-    message.textContent = "Player 1 venceu";
+    message.textContent = "Player 1 ganhou";
     choiceWinner.className = "win";
   } else if (playerWinner === 2) {
     choiceWinner.innerHTML = "";
     choiceWinner.appendChild(optionCPU.children[0].cloneNode(true));
-    message.textContent = "Computador venceu";
+    message.textContent = "Computador ganhou";
     choiceWinner.className = "win";
   } else {
     choiceWinner.innerHTML = "";
@@ -101,4 +117,36 @@ function results(optionPlayer1, optionCPU) {
     message.textContent = "Empate";
     choiceWinner.className = "draw";
   }
+  points(playerWinner);
+}
+
+function points(winner) {
+  message = false;
+
+  if(winner == 1) {
+     placarPlayer1 += 1;
+    if (placarPlayer1 == 3) {
+      message = "Player1 venceu!";
+      gameOver = true;
+    }
+  }
+  if(winner == 2) {
+    placarCPU += 1;
+    if (placarCPU == 3) {
+      message = "Computador venceu!";
+      gameOver = true;
+    }
+  }
+
+  if(message) {
+    setTimeout(() => {
+      finalAnnouncement.textContent = message;
+      optionsPlayer1.classList.add("hidden");
+    }, 2000);
+  }
+}
+
+function updateScore() {
+  player1ScoreElement.textContent = placarPlayer1;
+  cpuScoreElement.textContent = placarCPU;
 }
